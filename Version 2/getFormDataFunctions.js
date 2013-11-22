3db19617c33790ac348849166fb15e7b055b7c21
Amy Le <ale23@student.rcc.edu>
2013-11-21 22:48:29 -0800

    Version 2 of Personal Project
    Includes JavaScript for contact page and also for a preset randomizer.
    Still need JavaScript for custom randomizer.

---
+++ Version 2/getFormDataFunctions.js
@@ -0,0 +1,42 @@
+/** 
+	Amy Le
+	CIS 14A
+	Personal Project
+**/
+
+//now that have form that uses get method, name of field and its value appears in the url bar
+//need to access that and split to separate name from value, then assign into object that will later use
+
+function accessFormData (){
+	//create an empty object that will return with properties created from form data
+	var theFormObj = new Object ();
+	//gather the data from the url
+	var url = location.href;
+	//now need to split, if look at url, name and values begin after ?
+	//whenever split, an array is returned 
+	var listOfParameters = url.split("?");
+	//what is returned [ set 1, set 2], the parameters we want are in set 2
+	var parameter = listOfParameters[1].split("&");
+	//an array is returned [name=value, name=value]
+	//want to get the names by themselves and values by themselves to add into object
+	//need to loop through each value in parameter array and split accordingly
+	for (var i = 0; i < parameter.length; i ++){
+		var separateSets = parameter[i].split("=");
+		//returns [name, value] for each separateSet
+		//assign name and value to separate variables that will be used in object
+		var propName = separateSets[0];
+		var propValue = separateSets[1];
+		theFormObj[propName] = propValue;
+	}
+	return theFormObj;
+}
+
+function displayFormData(ourFormObject){
+	//since it is an object, obj { name: value, name: value}
+	//we want to display the name and value
+	//to do this need to loop through each property name in object
+	for (var propName in ourFormObject){
+		//document.write("<p>" + propName + ":" + ourFormObject[propName]+"</p>");
+		console.log(propName + " : " + ourFormObject[propName]);
+	}
+}
---
+++ Version 2/javascriptForDefault.js
@@ -0,0 +1,93 @@
+
+//create a constructor to create object for each food item
+
+function FoodItem (type, name){
+	this.cuisineType = type;
+	this.foodName = name;
+}
+//array of food objects
+var foodList = [
+	new FoodItem ("Chinese", "Dim Sum"),
+	new FoodItem ("Chinese", "Peking Duck"),
+	new FoodItem ("Chinese", "Fried Rice"),
+	new FoodItem ("Korean", "Korean BBQ"),
+	new FoodItem ("Korean", "Bibimbap"),
+	new FoodItem ("Korean", "Yangnyeom Tongdak"),
+	new FoodItem ("Japanese","Sushi"),
+	new FoodItem ("Japanese", "Ramen"),
+	new FoodItem ("Japanese", "Gyudon"),
+	new FoodItem ("Vietnamese", "Pho"),
+	new FoodItem ("Vietnamese", "Banh Xeo"),
+	new FoodItem ("Vietnamese","Goi Cuon"),
+	new FoodItem ("Indian", "Curry"),
+	new FoodItem ("Indian", "Tandoori Chicken"),
+	new FoodItem ("Indian", "Malai Koftak"),
+	new FoodItem ("Middle Eastern","Falafel"),
+	new FoodItem ("Middle Eastern","Tahini"),
+	new FoodItem ("Middle Eastern","Foul Mudammes"),
+	new FoodItem ("Greek","Gyro"),
+	new FoodItem ("Greek","Baklava"),
+	new FoodItem ("Greek","Spanakopita"),
+	new FoodItem ("Italian","Polenta and Gruyere"),
+	new FoodItem ("Italian","Pasta"),
+	new FoodItem ("Italian","Prosciutto Sandwich"),
+	new FoodItem ("Mexican","Burrito"),
+	new FoodItem ("Mexican","Taco"),
+	new FoodItem ("Mexican","Quesadilla")
+];
+
+//validator not needed because not choosing an option will result in randomizer without a filter
+//gives user choice to filter or not to filter 
+
+
+//for filtering need to get value that user has indicated for type of food
+//acquire food type from the filter
+function getFoodType () {
+	var url = location.href;
+	var myParam = url.split("?");
+	var NamVal = myParam[1].split("=");
+	//now have array [name, value]
+	return NamVal[1];
+}
+//now need way to compare the value (for filter yes) of the filter to object name in our foodList array 
+//in order to be more specific for what radomizer can pick from
+function runFilteredRandomizer(){
+	var choice = getFoodType();
+	//loop through the array to match our choice, put matching choices into a new array and use that for randomization
+	var filteredFoodList = [];
+	for (var i = 0; i < foodList.length; i ++){
+		if( choice === foodList[i].cuisineType){
+			filteredFoodList.push(foodList[i]);
+		}
+	}
+	//now have our filtered list
+	//need to get random number to pick an object from that list, randomly
+	return getRandomFood(filteredFoodList);
+}
+
+//function to randomize with or without any filtering
+function getRandomFood (list){
+	var num = Math.floor(Math.random()*(list.length-1));
+	var result = list[num];
+	return result;
+}
+
+
+function filterDetermination () {
+	//check to see if decided to go without filter and run 
+	if (getFoodType() === "Select"){
+		var chosen = getRandomFood (foodList);
+		document.write("<p>The randomizer has spoken</p>" + "<p>" + chosen.foodName + "</p>");
+	//otherwise use filtering scenario	
+	}else {
+		var filteredResult = runFilteredRandomizer();
+		document.write("<p>The randomizer has decided your fate! With your help of course.</p>" + "<p>" + filteredResult.foodName + "</p>");
+	}
+}
+
+
+
+
+
+
+
