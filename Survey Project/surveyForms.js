ebd4984dba72425f38e604cf7467711c9e4391b3
Ale2496063 <ale23@student.rcc.edu>
2013-11-27 17:43:04 -0800

    Signed-off-by: Ale2496063 <ale23@student.rcc.edu>

---
+++ Survey Project/surveyForms.js
@@ -0,0 +1,5 @@
+//need a function to validate our survey and ensure that all entries have been filled out properly
+function validateForm(){
+	
+}
+//need a function that grabs values from our submitted form
