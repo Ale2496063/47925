71812297b29c7a9868684795895502f6a7f588db
Amy Le <ale23@student.rcc.edu>
2013-12-02 23:18:13 -0800

    creating function to validate radio/checkboxes
    Incomplete function intended to do validation. Wrote comments to help
    guide development of function(s)

--- Survey Project/surveyForms.js
+++ Survey Project/surveyForms.js
@@ -1,5 +1,25 @@
+//CIS 14A 
+//Amy Le
+
 //need a function to validate our survey and ensure that all entries have been filled out properly
-function validateForm(){
+
+//For Survey 1
+
+function checkButtonSelected(){
+
+//to validate radio buttons, take into consideration that all options share name
+//need to ensure that one value is selected per radio name
+
+//get all form radio buttons, will be listed in array
+//format is formName.radioName
 	
+
 }
+
+
+
+
+
+
+
 //need a function that grabs values from our submitted form
