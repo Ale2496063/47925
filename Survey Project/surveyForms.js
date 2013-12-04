a0da4c4dc0bbe48e08072b4ebdbfb8fbdd937a76
Amy Le <ale23@student.rcc.edu>
2013-12-04 00:24:19 -0800

    function to validate checkboxes
    checkbox validation for survey1.
    need to adjust function name for radio check, put both radio check and
    checkbox validation functions into on function

--- Survey Project/surveyForms.js
+++ Survey Project/surveyForms.js
@@ -5,6 +5,7 @@
 
 //For Survey 1
 
+//change this function name to reflect that it only targets radio buttons
 function checkButtonsSurvey1(formName){
 	var formAccept = false;
 	var questionStatus = [];
@@ -24,7 +25,6 @@ function checkButtonsSurvey1(formName){
 				break;
 			}
 		}
-		
 	}
 	//since created array of true booleans related to radio questions, must have length of 7 to be valid
 	if ( questionStatus.length === 7){
@@ -35,6 +35,27 @@ function checkButtonsSurvey1(formName){
 	return formAccept;
 }
 
+//create function to check if at least one checkbox is checked for validation
+function validateCheckboxes(formName){
+	//similar to radio button validation, have array that will push values into
+	var questionStatus = [];
+	var checkboxesValid = false;
+	//array of checkbox names
+	var checkboxList = ["aboutme_cry", "aboutme_smile", "aboutme_streak", "aboutme_mad"];
+	for (var i = 0; i < checkboxList.length; i ++){
+		//for each checkbox, check to see if checked, it true, push boolean to array
+		if (formName[checkboxList[i].checked]){
+			questionStatus.push(checkboxList[i].checked);
+		}
+	}
+	//now check to see if questionStatus array length is greater or equal to 1, if so, then change checboxesValid to true
+	if ( questionStatus.length >= 1){
+		checkboxesValid = true;
+	}else{
+		alert("For question 8 please select at least one choice");
+	}
+	return checkboxesValid;
+}
 
 
 
