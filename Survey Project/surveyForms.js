6517f9e1706c220e147204f10546c254c8d32b0b
Amy Le <ale23@student.rcc.edu>
2013-12-05 01:23:26 -0800

    Survey1 form validation & form data into object
    Completed the form validation of survey1 which includes radios,
    checkboxes, and text fields. Also completed function that gets form data
    and places name value pairs into an object.

--- Survey Project/surveyForms.js
+++ Survey Project/surveyForms.js
@@ -3,10 +3,10 @@
 
 //need a function to validate our survey and ensure that all entries have been filled out properly
 
-//For Survey 1
+/*************************	For Survey 1 Validation	*********************/
 
-//change this function name to reflect that it only targets radio buttons
-function checkButtonsSurvey1(formName){
+//function to validate radios
+function validateRadiosSurvey1(formName){
 	var formAccept = false;
 	var questionStatus = [];
 	//will check radio buttons first to see if selected
@@ -35,6 +35,8 @@ function checkButtonsSurvey1(formName){
 	return formAccept;
 }
 
+
+
 //create function to check if at least one checkbox is checked for validation
 function validateCheckboxes(formName){
 	//similar to radio button validation, have array that will push values into
@@ -44,7 +46,7 @@ function validateCheckboxes(formName){
 	var checkboxList = ["aboutme_cry", "aboutme_smile", "aboutme_streak", "aboutme_mad"];
 	for (var i = 0; i < checkboxList.length; i ++){
 		//for each checkbox, check to see if checked, it true, push boolean to array
-		if (formName[checkboxList[i].checked]){
+		if (formName[checkboxList[i]].checked){
 			questionStatus.push(checkboxList[i].checked);
 		}
 	}
@@ -59,7 +61,68 @@ function validateCheckboxes(formName){
 
 
 
+//create function that will validate fields that take in text
+function validateText(formName){
+	var textValid = true;
+	var questionStatus = [];
+	var textFieldNames = ["random", "why"];
+	//create regex that will be used for comparison for BOTH sections requiring text
+	//accept almost any content
+	var textPattern = new RegExp ("[A-Z, a-z, 0-9, \w,\!,\#,\$,\%,\&,\',\*,\+,\-,\/,\=,\?,\^,\_,\`,\{,\|,\},\~]+");
+	//get values at these text fields
+	for (var i = 0; i < textFieldNames.length; i ++){
+		var fieldName = textFieldNames[i];
+		var fieldVal = document.survey1[fieldName].value;
+		//compare each value to regex and push a false into our questionStatus array
+		var fieldResult = textPattern.exec(fieldVal);
+		if (fieldResult == null){
+			questionStatus.push(false);
+		}
+		//if that fieldName does not match, false boolean pushed into array
+		//if our questionStatus array has a length >= 1, means at least one field empty, change textValid to false and return	
+	}
+	if (questionStatus.length >= 1){
+		alert("Question(s) 9 and/or 10 are incomplete!")
+		textValid = false;
+	}
+	return textValid;
+}
+
+
+//create function that will run all validation functions for survey1
+function validateSurvey1(formName){
+	//if either validator returns false, do not allow form to submit
+	if(!validateRadiosSurvey1(formName) || !validateCheckboxes(formName) || !validateText(formName)){
+	//then run this block
+		return false;
+	}
+	return true;
+}
 
 
 
-//need a function that grabs values from our submitted form
+
+
+
+/*************************	To create object from our Form values	*********************/
+
+function getSubmittedForm(){
+	//create object to store our form keys and their corresponding properties
+	var submittedFormObj = new Object();
+	//get data from url
+	var url = location.href;
+	//now need to split at "?" to get our array with our name/values at index 2
+	var urlSplitArray = url.split("?");
+	var paramList = urlSplitArray[1];
+	//split our paramList at "&" to get separate name value pair sets in array
+	var namValSets = paramList.split("&");
+	//now need to loop through this array and split into name and into value, assign to vars for use into putting in object
+	for (var i = 0; i < namValSets.length; i ++){
+		var sepNameAndVal = namValSets[i].split("=");
+		//for each have array [name, value]
+		var prop = sepNameAndVal[0];
+		var val = sepNameAndVal[1];
+		submittedFormObj[prop] = val;
+	}
+	return submittedFormObj;
+}
